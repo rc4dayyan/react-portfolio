344150763831a9dbf22e3fbd9701856832e5935b
rc4dayyan <daeytea@gmail.com>
2020-08-07 15:59:32 +0700

    add live preview to readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Live Preview
+[https://rc4dayyan.github.io/react-portfolio](https://rc4dayyan.github.io/react-portfolio)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
         // Smooth scrolling using jQuery easing
         $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
             if (
-                window.location.pathname.replace(/^\//, "") ==
+                window.location.pathname.replace(/^\//, "") ===
                     this.pathname.replace(/^\//, "") &&
-                window.location.hostname == this.hostname
+                window.location.hostname === this.hostname
             ) {
                 var target = $(this.hash);
                 target = target.length
